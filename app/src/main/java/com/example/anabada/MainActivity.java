7bf321e5d2ff70a9d36621c74ae705169c784d32
NAMSOO-KIM <golden3333@hanmail.net>
2019-07-29 23:37:07 +0900

    Login commit

--- app/src/main/java/com/example/anabada/MainActivity.java
+++ app/src/main/java/com/example/anabada/MainActivity.java
@@ -4,12 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,24 +34,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        noticeListView =(ListView)findViewById(R.id.noticeListView);
-        noticeList=new ArrayList<Notice>();
-        noticeList.add(new Notice("공지사항입니다.","호감도상승","2019-07-18"));
+        noticeListView = (ListView) findViewById(R.id.noticeListView);
+        noticeList = new ArrayList<Notice>();
 
-        noticeList.add(new Notice("님들 하이욤.","호감도상승","2019-07-18"));
-        noticeList.add(new Notice("발소리 작게좀요.","호감도상승","2019-07-18"));
-        noticeList.add(new Notice("한이음 쪼아!.","호감도상승","2019-07-18"));
-        noticeList.add(new Notice("공지사항입니다.","호감도상승","2019-07-18"));
-        noticeList.add(new Notice("공지사항입니다.","호감도상승","2019-07-18"));
-        adapter= new NoticeListAdapter(getApplicationContext(),noticeList);
-        noticeListView.setAdapter(adapter);
+        adapter = new NoticeListAdapter(getApplicationContext(), noticeList);
+        //noticeListView.setAdapter(adapter);
 
 
-        final Button roomButton = (Button)findViewById(R.id.roomButton);
-        final Button scheduleButton = (Button)findViewById(R.id.scheduleButton);
-        final Button GameButton = (Button)findViewById(R.id.GameButton);
+        final Button roomButton = (Button) findViewById(R.id.roomButton);
+        final Button scheduleButton = (Button) findViewById(R.id.scheduleButton);
+        final Button GameButton = (Button) findViewById(R.id.GameButton);
         final LinearLayout notice = (LinearLayout) findViewById(R.id.notice);
-
 
 
         roomButton.setOnClickListener(new View.OnClickListener() {
@@ -55,11 +57,11 @@ public class MainActivity extends AppCompatActivity {
 
                 FragmentManager fragmentManager = getSupportFragmentManager(); //Fragment 관리해주는 역할
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();//Fragment트랜잭션은 Fragment 연산에 관련된 작업
-                fragmentTransaction.replace(R.id.fragment,new RoomFragment()); //fragment 부분을 RoomFragment로 대체해주는것
+                fragmentTransaction.replace(R.id.fragment, new RoomFragment()); //fragment 부분을 RoomFragment로 대체해주는것
                 fragmentTransaction.commit();
             }
         });
-
+        // new BackgroundTask().execute(); //DB 접근해서 찾아봄
         scheduleButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -70,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
 
                 FragmentManager fragmentManager = getSupportFragmentManager();
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                fragmentTransaction.replace(R.id.fragment,new ScheduleFragment());
+                fragmentTransaction.replace(R.id.fragment, new ScheduleFragment());
                 fragmentTransaction.commit();
             }
         });
@@ -85,9 +87,81 @@ public class MainActivity extends AppCompatActivity {
 
                 FragmentManager fragmentManager = getSupportFragmentManager(); //Fragment 관리해주는 역할
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();//Fragment트랜잭션은 Fragment 연산에 관련된 작업
-                fragmentTransaction.replace(R.id.fragment,new GameFragment()); //fragment 부분을 RoomFragment로 대체해주는것
+                fragmentTransaction.replace(R.id.fragment, new GameFragment()); //fragment 부분을 RoomFragment로 대체해주는것
                 fragmentTransaction.commit();
             }
         });
+        new BackgroundTask().execute();
     }
-}
+
+        class BackgroundTask extends AsyncTask<Void, Void, String>
+        {
+            String target;
+           @Override
+           protected void onPreExecute(){
+               target = "http://gksdldma.cafe24.com/NoticeList.php";
+
+           }
+
+            @Override
+            protected String doInBackground(Void... voids) {
+               try{
+                URL url = new URL(target);
+                   HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+                    InputStream inputStream = httpURLConnection.getInputStream();
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                    String temp;
+                    StringBuilder stringBuilder = new StringBuilder();
+                    while((temp = bufferedReader.readLine()) !=null){
+                         stringBuilder.append(temp+"\n");
+
+                    }
+                    bufferedReader.close();
+                    inputStream.close();
+                    httpURLConnection.disconnect();
+                    return stringBuilder.toString().trim();
+
+               }
+               catch(Exception e){
+            e.printStackTrace();
+               }
+
+                return null;
+            }
+            @Override
+            public void onProgressUpdate(Void... values){
+               super.onProgressUpdate();
+
+            }
+            @Override
+            public void onPostExecute(String result){
+               try{
+                   JSONObject jsonObject = new JSONObject(result);
+                   JSONArray jsonArray = jsonObject.getJSONArray("response");
+                   int count =0;
+                   String noticeContent, noticeName, noticeDate;
+                   while(count < jsonArray.length()){
+                       JSONObject object = jsonArray.getJSONObject(count);
+                       noticeContent= object.getString("noticeContent");
+                       noticeName= object.getString("noticeName");
+                       noticeDate= object.getString("noticeDate");
+                       Notice notice = new Notice(noticeContent, noticeName, noticeDate);
+                      noticeListView.setAdapter(adapter); //추가됨
+                       noticeList.add(notice);
+                       adapter.notifyDataSetChanged();
+
+                       count++;
+
+
+                   }
+               }
+               catch (Exception e){
+                e.printStackTrace();
+
+               }
+
+            }
+        }
+    }
+
+
