62c56baa4c9b49c28fac8e99f401a2b7c8cf638d
NAMSOO-KIM <golden3333@hanmail.net>
2019-07-30 20:46:25 +0900

    checkpoint

--- app/src/main/java/com/example/anabada/MainActivity.java
+++ app/src/main/java/com/example/anabada/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Toast;
 
 
 import org.json.JSONArray;
@@ -199,6 +200,20 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+    private long lastTimeBackPressed;
+
+//    뒤로가기 2번 누를시 종료
+    @Override
+    public void onBackPressed(){
+        if(System.currentTimeMillis()-lastTimeBackPressed<1500)
+        {
+            finish();
+            return;
+
+        }
+        Toast.makeText(this,"'뒤로'버튼 한번 더 누르면 종료.", Toast.LENGTH_SHORT);
+        lastTimeBackPressed=System.currentTimeMillis();
     }
 }
 
