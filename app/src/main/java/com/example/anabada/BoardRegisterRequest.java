8505d5d9450d4701ebcc6e40767ee279c6809388
김남수 <aowlrehrk3@naver.com>
2019-08-22 02:25:25 +0900

    Board register

--- app/src/main/java/com/example/anabada/BoardRegisterRequest.java
+++ app/src/main/java/com/example/anabada/BoardRegisterRequest.java
@@ -7,15 +7,17 @@ import com.android.volley.toolbox.StringRequest;
 import java.util.HashMap;
 import java.util.Map;
 
-//유저 등록
-public class RegisterRequest extends StringRequest {
-    final static private String URL = "http://gksdldma.cafe24.com/UserRegisterRequest.php";
+//방 생성 클래스
+public class BoardRegisterRequest extends StringRequest {
+    final static private String URL = "http://gksdldma.cafe24.com/BoardRegister.php";
     private Map<String , String> parameters;
-    public RegisterRequest(String userName, Response.Listener<String> listener )
+    public BoardRegisterRequest(String boardTitle, String boardContents, Response.Listener<String> listener )
     {
         super(Method.POST , URL, listener,null); //URL에 POST 방식(요청을 숨겨서)으로 보내라.
         parameters = new HashMap<>(); //각각의 값을 넣을 수 있게 해쉬맵으로
-        parameters.put("userName",userName);
+        parameters.put("boardTitle",boardTitle);
+        parameters.put("boardContents",boardContents);
+
 
     }
     @Override
--- app/src/main/java/com/example/anabada/Product_regist.java
+++ app/src/main/java/com/example/anabada/Product_regist.java
@@ -2,8 +2,19 @@ package com.example.anabada;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AlertDialog;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Product_regist extends AppCompatActivity {
 
@@ -11,6 +22,66 @@ public class Product_regist extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_regist);
+
+        //idText 는 방 제목
+        //passwordText 는 방 비밀번호
+        final EditText TitleText = (EditText)findViewById(R.id.editText6_title);
+        final EditText ContentsText = (EditText)findViewById(R.id.editText6_description);
+
+        Button BoardRegisterButton = (Button) findViewById(R.id.button6_registration);
+        BoardRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String boardTitle = TitleText.getText().toString();
+                String boardContents= ContentsText.getText().toString();
+
+                //Lister에서 원하는 결과값 다룰수 있게
+                Response.Listener<String> responseListener = new Response.Listener<String>(){
+
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if (success) {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(Product_regist.this);
+                                builder.setMessage("등록 완료!")
+                                        .setPositiveButton("확인", null)
+                                        .create()
+                                        .show();
+
+
+                                Intent intent = new Intent(Product_regist.this, MainActivity.class);
+                                Product_regist.this.startActivity(intent);
+                            }
+
+                            else{
+                                AlertDialog.Builder builder = new AlertDialog.Builder(Product_regist.this);
+                                builder.setMessage("등록 실패!")
+                                        .setNegativeButton("확인",null)
+                                        .create()
+                                        .show();
+
+                            }
+
+                        }
+                        catch (JSONException e)
+                        {
+                            e.printStackTrace();
+                        }
+
+                    }
+                };
+
+               BoardRegisterRequest boardRegisterRequest = new BoardRegisterRequest(boardTitle, boardContents, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(Product_regist.this);
+                queue.add(boardRegisterRequest); //버튼 클릭시 roomRegisterRequest 실행
+
+
+            }
+        });
+    }
+    //회원 등록 창 꺼지면 실행
+
     }
 
-}
